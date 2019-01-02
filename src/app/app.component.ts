56ff14f6cba802c25e314e87cde69ed51ed073bd
Eduardo Denis <denise@zarate.gob.ar>
2019-01-02 13:26:10 -0300

    Tipos de Data Binding y String Interpolation. Ejercicio.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+class Person {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName  = lastName;
+    }
+    greet() {
+        return `Hola ${this.firstName} ${this.lastName}`;
+    }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +19,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Platzi Square';
+  ready: boolean = false;
+
+  person: Person = new Person('Eduardo', 'Denis')
+
+  constructor(){
+    setTimeout(() => {
+      this.ready = true;
+    }, 3000)
+  }
+
+  doSomething(){
+    alert(this.person.greet());
+  }
 }
