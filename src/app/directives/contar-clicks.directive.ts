820fe754c677c9cf5627b29a13db22fdd24f4c11
Eduardo Denis <denise@zarate.gob.ar>
2019-01-03 13:15:38 -0300

    HostBinding Decorate on Directive attribute

--- src/app/directives/contar-clicks.directive.ts
+++ src/app/directives/contar-clicks.directive.ts
@@ -1,17 +1,20 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
-  selector: 'a[appContarClicks]'
+  selector: 'li[appContarClicks]'
 })
 export class ContarClicksDirective {
 
   constructor() { }
 
   numberOfClicks = 0;
+  @HostBinding('style.opacity') opacity: number = .5;
 
   @HostListener('click', ['$event.target'])
   onClick(btn) {
     console.log('button', btn, 'number of clicks:', this.numberOfClicks++);
+    console.log(this.opacity);
+    this.opacity =  this.opacity >= 1.0 ? .5 : this.opacity + .1;
   }
 
 }
