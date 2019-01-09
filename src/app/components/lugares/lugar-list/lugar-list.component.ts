c7c4fe2331ea271e89f17dd97df1d1af7edf4f58
TheEdu <edigde@gmail.com>
2019-01-09 08:50:36 -0300

    Animaciones para lugares destacados

--- src/app/components/lugares/lugar-list/lugar-list.component.ts
+++ src/app/components/lugares/lugar-list/lugar-list.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { LugaresService } from '../../../services/lugar.service';
+import {trigger, state, style, transition, animate} from "@angular/animations";
 
 @Component({
   selector: 'app-lugar-list',
   templateUrl: './lugar-list.component.html',
-  styleUrls: ['./lugar-list.component.css']
+  styleUrls: ['./lugar-list.component.css'],
+  animations: [
+        trigger('contenedorAnimable', [
+            state('inicial', style({
+                opacity: 0
+            })),
+            state('final', style({
+                opacity: 1
+            })),
+            transition('inicial => final', animate(2000)),
+            transition('final => inicial', animate(1000)),
+        ])
+    ]
 })
 export class LugarListComponent {
   title = 'Platzi Square';
@@ -12,11 +25,16 @@ export class LugarListComponent {
   lat = 51.678418;
   lng = 7.809007;
   zoom = 18;
+  state = 'inicial';
 
   constructor( private lugaresService: LugaresService ) {
     lugaresService.getLugares()
                   .subscribe( places => {
                     this.places = places;
+                    this.state = 'final';
+                  }, error => {
+                      console.log(error);
+                      alert('Tenemos algo de dificultades, disculpe las molestias. Error: ' + error.statusText);
                   });
   }
 }
